0e4fc1a21172d41920af6a8727ec02d0658511cf
Alexey Levashov <levashov.alexey@gmail.com>
2018-05-22 10:03:38 +0300

    Initial commit

---
+++ PeerToPeerNetwork/PeerToPeerNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeerToPeerNetwork
+//
+//  Created by Alexey Levashov on 5/22/18.
+//  Copyright © 2018 Alexey Levashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
